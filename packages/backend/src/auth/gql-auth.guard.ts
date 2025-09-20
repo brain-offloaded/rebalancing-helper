7c31d38424687d5a768d07886ccd6611e37844bd
brain-offloaded <brain.offloaded@gmail.com>
2025-09-21 01:39:35 +0900

    타입 에러 수정

--- packages/backend/src/auth/gql-auth.guard.ts
+++ packages/backend/src/auth/gql-auth.guard.ts
@@ -5,27 +5,31 @@ import {
 } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
+import type { Request } from 'express';
 import { GraphqlContext } from '../common/graphql/graphql-context.type';
 import { ActiveUserData } from './auth.types';
 
 @Injectable()
 export class GqlAuthGuard extends AuthGuard('jwt') {
-  override getRequest(context: ExecutionContext) {
+  override getRequest(context: ExecutionContext): Request {
     const gqlExecutionContext = GqlExecutionContext.create(context);
     return gqlExecutionContext.getContext<GraphqlContext>().req;
   }
 
-  override handleRequest(
+  override handleRequest<TUser extends ActiveUserData = ActiveUserData>(
     err: unknown,
-    user: ActiveUserData | null,
+    user: TUser | null,
     info: unknown,
     context: ExecutionContext,
-  ) {
+    _status?: unknown,
+  ): TUser {
     if (err || !user) {
+      // eslint-disable-next-line @typescript-eslint/only-throw-error
       throw err ?? new UnauthorizedException();
     }
 
-    const gqlContext = GqlExecutionContext.create(context).getContext<GraphqlContext>();
+    const gqlContext =
+      GqlExecutionContext.create(context).getContext<GraphqlContext>();
     gqlContext.user = user;
 
     return user;
