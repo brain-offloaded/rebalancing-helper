de48e02818f53e48342bfbcc9d14fb8a978e88ed
brain-offloaded <brain.offloaded@gmail.com>
2025-09-21 14:21:34 +0900

    cors 문제 해결

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -4,6 +4,12 @@ import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'OPTIONS'],
+  });
   const prismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app);
   await app.listen(process.env.PORT ?? 3000);
