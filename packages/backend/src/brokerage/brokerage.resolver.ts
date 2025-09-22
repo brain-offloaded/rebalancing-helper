a3ba0e20c757f183bb786cd4eeaead6865431813
leejunsung <brain.offloaded@gmail.com>
2025-09-23 00:13:34 +0900

    Revert "수동 보유 수량 조정 및 현재가 동기화 기능 추가"

--- packages/backend/src/brokerage/brokerage.resolver.ts
+++ packages/backend/src/brokerage/brokerage.resolver.ts
@@ -9,9 +9,6 @@ import {
 import {
   CreateBrokerInput,
   CreateBrokerageAccountInput,
-  PatchHoldingQuantityInput,
-  PutHoldingQuantityInput,
-  SyncHoldingPriceInput,
   UpdateBrokerInput,
   UpdateBrokerageAccountInput,
 } from './brokerage.dto';
@@ -108,29 +105,5 @@ export class BrokerageResolver {
     @Args('accountId') accountId: string,
   ): Promise<BrokerageHolding[]> {
     return this.brokerageService.refreshHoldings(user.userId, accountId);
-  }
-
-  @Mutation(() => BrokerageHolding)
-  patchBrokerageHoldingQuantity(
-    @CurrentUser() user: ActiveUserData,
-    @Args('input') input: PatchHoldingQuantityInput,
-  ): Promise<BrokerageHolding> {
-    return this.brokerageService.patchHoldingQuantity(user.userId, input);
-  }
-
-  @Mutation(() => BrokerageHolding)
-  putBrokerageHoldingQuantity(
-    @CurrentUser() user: ActiveUserData,
-    @Args('input') input: PutHoldingQuantityInput,
-  ): Promise<BrokerageHolding> {
-    return this.brokerageService.putHoldingQuantity(user.userId, input);
-  }
-
-  @Mutation(() => BrokerageHolding)
-  syncBrokerageHoldingPrice(
-    @CurrentUser() user: ActiveUserData,
-    @Args('input') input: SyncHoldingPriceInput,
-  ): Promise<BrokerageHolding> {
-    return this.brokerageService.syncHoldingPrice(user.userId, input);
   }
 }
